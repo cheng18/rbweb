c1179b8481f00459e7ab740b7fd756eee1fc7616
cheng18 <hao18w@gmail.com>
2018-10-06 01:40:51 +0800

    add input page

---
+++ src/components/Input.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            :counter="10"
+            label="Name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-select
+            v-model="select"
+            :items="items"
+            :rules="[v => !!v || 'Item is required']"
+            label="Item"
+            required
+          ></v-select>
+          <v-checkbox
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to continue!']"
+            label="Do you agree?"
+            required
+          ></v-checkbox>
+
+          <v-btn
+            :disabled="!valid"
+            @click="submit"
+          >
+            submit
+          </v-btn>
+          <v-btn @click="clear">clear</v-btn>
+        </v-form>`
+
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'I',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
